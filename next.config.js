a6fbdd195b44153424220f663046de7dff3be086
rezaan6 <rezaan6@gmail.com>
2022-09-23 01:27:29 +0530

    Progress to 'Creating User Store'

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'images.unsplash.com',
+      'cdn0.iconfinder.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
