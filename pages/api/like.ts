bfcede6f4e88a96c3bea4f825bc96c537fccb577
rezaan6 <rezaan6@gmail.com>
2022-09-27 22:24:14 +0530

    --- Git Commit ---

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -2,9 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { uuid } from "uuidv4";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const user = req.body;
-
-    client.createIfNotExists(user).then(() => res.status(200).json("Login Success"));
+  if (req.method === "PUT") {
+    const {}
   }
 }
