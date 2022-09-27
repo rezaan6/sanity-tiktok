57b1368dc4dcde0fa666237018bf6b47edfc9f52
rezaan6 <rezaan6@gmail.com>
2022-09-27 22:21:58 +0530

    --- Git Commit ---

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -26,7 +26,7 @@ const LikeButton = ({handleDislike, handleLike}:IProps) => {
         </div>
         )}
 
-        <p className="text-base font-semibold">likes</p>
+        <p className="text-base font-semibold">{likes?.length}</p>
     </div>
 </div>
   );
