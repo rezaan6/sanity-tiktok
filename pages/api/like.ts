e0a69dc74994c44cd92524ab78afd0f34c85153a
rezaan6 <rezaan6@gmail.com>
2022-09-27 22:24:59 +0530

    --- Git Commit ---

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { uuid } from "uuidv4";
 
-import {client} 
+import {client} from '../../utils/'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "PUT") {
