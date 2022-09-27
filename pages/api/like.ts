da0265d6fe1efc9396a25e75a80b7b93893e6ba7
rezaan6 <rezaan6@gmail.com>
2022-09-27 22:25:14 +0530

    --- Git Commit ---

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { uuid } from "uuidv4";
 
-import {client} from '../../utils/'
+import { client } from "../../utils/client";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "PUT") {
-    const {userId, postId, like = req.body}
+    const { userId, postId, like}    = req.body ;
   }
 }
