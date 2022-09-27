349dc24b217e6a882744cc0076ddf4cafff0e08c
rezaan6 <rezaan6@gmail.com>
2022-09-27 22:18:27 +0530

    --- Git Commit ---

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -132,9 +132,7 @@ const Detail = ({ postDetails }: IProps) => {
           <p className="px-10 text-lg text-gray-600">{post.caption}</p>
 
           <div className="mt-10 px-10">{ userProfile &&(
-            <LikeButton handleLike={() => handleLike(true)} handleDislike={() => {
-              false
-            }}/>
+            <LikeButton handleLike={() => handleLike(true)} handleDislike={() =>handleLi}/>
 
           )}</div>
 
