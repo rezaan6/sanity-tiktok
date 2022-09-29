97a7ea61cb5e572d52edcccdd1a3e23182424a72
rezaan6 <rezaan6@gmail.com>
2022-09-29 21:30:07 +0530

    --- Git Commit ---

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Dispatch, SetStateAction} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { GoVerified } from "react-icons/go";
@@ -6,23 +6,38 @@ import { GoVerified } from "react-icons/go";
 import useAuthStore from "../store/authStore";
 import NoResults from "./NoResults";
 
-const Comments = () => {
-  const comments = [];
+interface IProps {
+  comment: string;
+  setComment: Dispatch<SetStateAction>;
+  addComment: () => void;
+  comments: IComment[];
+  isPostingComment: boolean;
+}
+
+interface IComment {
+  comment: string;
+  length?: number;
+  _key: string;
+  postedBy: { _ref: string; _id: string };
+}
+
+const Comments = ({comment, setComment,addComment,comments ,isPostingComment }:IProps) => {
+
   const { userProfile } = useAuthStore();
-  const isPostingComment = false;
+
 
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
-        {comments.length ? <div className="">videos</div> : <NoResults text="No comments yet" />}
+        {comments?.length ? <div className="">videos</div> : <NoResults text="No comments yet" />}
       </div>
 
       {userProfile && (
         <div className="absolute bottom-0 left-0 pb-6 px-2 md:px-10">
           <form onSubmit={() => {}} className="flex gap-4">
             <input
-              value={""}
-              onChange={() => {}}
+              value={comment}
+              onChange={(e) => setComment(e.target.value.trim)}
               placeholder="Add comment..."
               className="bg-primary px-6 py-4 text-base font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray focus:outline-none focus:border-gray-300 flex-1 rounded-lg "
             />
