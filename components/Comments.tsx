bf282210ecbe158f9ab33f0fc5291aff620d1f00
rezaan6 <rezaan6@gmail.com>
2022-09-29 20:59:36 +0530

    --- Git Commit ---

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -8,11 +8,21 @@ import NoResults from "./NoResults";
 
 const Comments = () => {
   const comments = [];
+  const { userProfile } = useAuthStore();
+
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
-        {comments.length ? <div className="">videos</div> : <NoResults text="No comments yet! Be the first one to add a commment." />}
+        {comments.length ? <div className="">videos</div> : <NoResults text="No comments yet" />}
       </div>
+
+      {userProfile && (
+        <div className="absolute botttom-0 left-0 pb-6 px-2 md:px-10">
+          <form onSubmit={() => {}} className="flex gap-4">
+            <input type="text" value={""} onChange={() => {}} placeholder="Add comment..." className="bg-primary px-6 py-4 text-base font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray focus:outline-none focus:border-gray-300 flex-1 round " />
+          </form>
+        </div>
+      )}
     </div>
   );
 };
--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { MdOutlineVideoCamOff } from "react-icons/md";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
+
 interface IProps {
   text: string;
 }
 
 const NoResults = ({ text }: IProps) => {
-  return <div className="flex flex-col justify-center items-center w-full h-full">
-    <p className="text-8xl">
-      <MdOutlineVideoCamOff/>
-    </p>
-    <p className="text"></p>
-
-  </div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <p className="text-8xl">
+        {text === "No comments yet" ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
