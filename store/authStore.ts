de5693ae084cc2c6085809d8be492615157070ce
Rezaan Riyaz <rezaan6@gmail.com>
2022-11-05 17:54:39 +0530

    --- Git Commit ---

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,12 +2,13 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
 
-import .meta. const BASE_URL 
+import { BASE_URL } from "../utils"; 
 const authStore = (set: any) => ({
   userProfile: null,
 
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: ()=>set({userProfile:null})
+  
 });
 
 const useAuthStore = create(persist(authStore, { name: "auth" }));
