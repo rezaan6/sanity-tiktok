a6d612e5b8bee68992ed29a489d940c2f2679930
Rezaan Riyaz <rezaan6@gmail.com>
2022-11-05 18:25:10 +0530

    --- Git Commit ---

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import React, { useEffect } from 'react'
 import useAuthStore from '../store/authStore'
+import { IUser } from '../types';
 
 const SuggestedAccounts = () => {
   
@@ -12,7 +14,15 @@ const SuggestedAccounts = () => {
   
 
   return (
-    <div>SuggestedAccounts</div>
+    <div className="xl:border-b-2 border-gray-200 pb-4">
+      <p className='   '>Suggested Accounts</p>
+
+      <div className="">{
+        allUsers.slice(0,6).map((user:IUser) =>(
+          <Link href={`/profile/${user._id}`} key={user._id}></Link>
+        ))
+      }</div>
+    </div>
 
   )
 }
