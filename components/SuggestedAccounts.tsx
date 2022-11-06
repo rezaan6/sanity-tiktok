afef0dc0bc6c626faf7f7617bb65dbad7755599c
Rezaan Riyaz <rezaan6@gmail.com>
2022-11-06 17:26:44 +0530

    --- Git Commit ---

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect } from 'react'
 import useAuthStore from '../store/authStore'
@@ -15,11 +16,23 @@ const SuggestedAccounts = () => {
 
   return (
     <div className="xl:border-b-2 border-gray-200 pb-4">
-      <p className='   '>Suggested Accounts</p>
+      <p>Suggested Accounts</p>
 
       <div className="">{
         allUsers.slice(0,6).map((user:IUser) =>(
-          <Link href={`/profile/${user._id}`} key={user._id}></Link>
+          <Link href={`/profile/${user._id}`} key={user._id}>
+            <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
+              <div className="w-8 h-6">
+                <Image src={user.image}
+                width={34}
+                height={34}
+                className="rounded-full"
+                alt="user profile"
+                layout="responsive"
+                />
+              </div>
+            </div>
+          </Link>
         ))
       }</div>
     </div>
