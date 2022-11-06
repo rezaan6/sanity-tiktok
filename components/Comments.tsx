50bc982146591b3d5be7d9aba0b4e79f7eaf2d3e
Rezaan Riyaz <rezaan6@gmail.com>
2022-11-06 17:47:04 +0530

    --- Git Commit ---

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -23,13 +23,13 @@ interface IComment {
 
 const Comments = ({comment, setComment,addComment,comments ,isPostingComment }:IProps) => {
 
-  const { userProfile } = useAuthStore();
+  const { userProfile, allUsers } = useAuthStore();
 
 
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
-        {comments?.length ? <div className="">videos</div> : <NoResults text="No comments yet" />}
+        {comments?.length ? comments.map(item,idx)=>())):( <NoResults text="No comments yet" />)}
       </div>
 
       {userProfile && (
--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,42 +1,47 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { useEffect } from 'react'
+import { GoVerified } from 'react-icons/go'
 import useAuthStore from '../store/authStore'
-import { IUser } from '../types';
+import { IUser } from '../types'
 
 const SuggestedAccounts = () => {
-  
-  const { fetchAllUsers, allUsers} = useAuthStore();
+  const { fetchAllUsers, allUsers } = useAuthStore()
 
   useEffect(() => {
-    fetchAllUsers();
-  
-  }, [fetchAllUsers]);
-  
+    fetchAllUsers()
+  }, [fetchAllUsers])
 
   return (
     <div className="xl:border-b-2 border-gray-200 pb-4">
       <p>Suggested Accounts</p>
 
-      <div className="">{
-        allUsers.slice(0,6).map((user:IUser) =>(
+      <div className="">
+        {allUsers.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
             <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
               <div className="w-8 h-6">
-                <Image src={user.image}
-                width={34}
-                height={34}
-                className="rounded-full"
-                alt="user profile"
-                layout="responsive"
+                <Image
+                  src={user.image}
+                  width={34}
+                  height={34}
+                  className="rounded-full"
+                  alt="user profile"
+                  layout="responsive"
                 />
+              </div>
+              <div className="hidden xl:block">
+                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                  {user.userName.replaceAll(' ', '')}
+                  <GoVerified className="text-blue-400" />
+                </p>
+                <p className="capitalize text-gray-400"></p>
               </div>
             </div>
           </Link>
-        ))
-      }</div>
+        ))}
+      </div>
     </div>
-
   )
 }
 
