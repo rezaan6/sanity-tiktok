eeb3cc726948d5f652c64fb7a45be94cb44ed4bc
Rezaan Riyaz <rezaan6@gmail.com>
2022-11-08 17:31:52 +0530

    Latest Files Updated

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,4 +1,6 @@
 import axios from "axios"
+import Image from "next/image"
+import { GoVerified } from "react-icons/go"
 import { BASE_URL } from "../../utils"
 
 interface IProps{
@@ -10,7 +12,29 @@ interface IProps{
 }
 
 const Profile = (data:IProps) => {
-  return <div className="">Profile</div>
+  const {user, userVideos, userLikedVideos} = data
+  return <div className="w-full">
+    <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+    <div className="w-8 h-6">
+                <Image
+                  src={user.image}
+                  width={34}
+                  height={34}
+                  className="rounded-full"
+                  alt="user profile"
+                  layout="responsive"
+                />
+              </div>
+              <div className="hidden xl:block">
+                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                  {user.userName.replaceAll(' ', '')}
+                  <GoVerified className="text-blue-400" />
+                </p>
+                <p className="capitalize text-gray-400"></p>
+              </div>
+    </div>
+    
+  </div>
 }
 
 export const getServerSideProps = async ({
@@ -18,7 +42,7 @@ export const getServerSideProps = async ({
 }: {
   params: { id: string }
 }) => {
-    const res = await axios.get(`${BASE_URL}/profile/${id}`)
+    const res = await axios.get(`${BASE_URL}/api/profile/${id}`)
 
     return {
       props:{data: res.data}
